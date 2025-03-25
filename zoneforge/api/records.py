5d6888ea366194dfe148f7394377cb7e0536435e
holysoles <holysoles97@gmail.com>
2025-03-25 14:32:22 -0500

    refactor: don't rely on current_api in core module

--- zoneforge/api/records.py
+++ zoneforge/api/records.py
@@ -1,4 +1,5 @@
 from flask_restx import Resource, Namespace, reqparse, fields
+from flask import current_app
 from werkzeug.exceptions import *  # pylint: disable=wildcard-import,unused-wildcard-import,redefined-builtin
 from zoneforge.core import (
     get_records,
@@ -103,6 +104,7 @@ class DnsRecord(Resource):
 
         records = get_records(
             zone_name=zone_name,
+            zonefile_folder=current_app.config["ZONE_FILE_FOLDER"],
             record_name=record_name,
             record_type=record_type,
             include_soa=include_soa,
@@ -120,6 +122,7 @@ class DnsRecord(Resource):
         args = record_post_parser.parse_args()
         new_record = create_record(
             zone_name=zone_name,
+            zonefile_folder=current_app.config["ZONE_FILE_FOLDER"],
             record_name=args["name"],
             record_type=args["type"],
             record_ttl=args["ttl"],
@@ -147,6 +150,7 @@ class SpecificDnsRecord(Resource):
 
         records = get_records(
             zone_name=zone_name,
+            zonefile_folder=current_app.config["ZONE_FILE_FOLDER"],
             record_name=record_name,
             record_type=record_type,
             include_soa=include_soa,
@@ -165,8 +169,11 @@ class SpecificDnsRecord(Resource):
         """
         args = record_put_parser.parse_args()
         record_ttl = args.get("ttl")
+        if record_ttl is None:
+            record_ttl = current_app.config["DEFAULT_ZONE_TTL"]
         updated_record = update_record(
             zone_name=zone_name,
+            zonefile_folder=current_app.config["ZONE_FILE_FOLDER"],
             record_name=record_name,
             record_type=args["type"],
             record_ttl=record_ttl,
@@ -185,6 +192,7 @@ class SpecificDnsRecord(Resource):
         args = record_delete_parser.parse_args()
         delete_record(
             zone_name=zone_name,
+            zonefile_folder=current_app.config["ZONE_FILE_FOLDER"],
             record_name=record_name,
             record_type=args["type"],
             record_data=args["data"],
